cd6b8ac732bb7cfc470eb6fd02ed211c9745ad09
bySabi Files <bysabi.files@gmail.com>
2016-07-07 19:01:23 +0200

    follow tape output syntax

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ var TAPReporter = function(baseReporterDecorator, config, logger, helper) {
     path = require('path'),
     fs = require('fs'),
     EOL = require('os').EOL,
-    numbers, outputFile;
+    numbers, outputFile,
+    currentSuite;
 
   /**
    * save all data that is coming in to the `data` variable for later use and
@@ -20,6 +21,14 @@ var TAPReporter = function(baseReporterDecorator, config, logger, helper) {
     }
   }
 
+  function writeSuite(suite) {
+    suite = suite.join(' ').replace(/\./g, '_');
+    if (currentSuite != suite) {
+      write(suite);
+      currentSuite = suite;
+    }
+  }
+
   if (tapReporterConfig.outputFile) {
     outputFile = path.resolve(config.basePath, tapReporterConfig.outputFile)
   }
@@ -28,40 +37,63 @@ var TAPReporter = function(baseReporterDecorator, config, logger, helper) {
 
   this.onRunStart = function() {
     numbers = new Object();
+    write('TAP version 13' + EOL);
   };
 
-   this.onBrowserStart = function(browser) {
-     numbers[browser.id] = 0;
+  this.onBrowserStart = function(browser) {
+    numbers[browser.id] = 0;
   };
 
   this.specSuccess = function(browser, result) {
-    write("ok " + ++numbers[browser.id] + " " + result.suite.join(' ').replace(/\./g, '_') + " " + result.description + EOL);
+    writeSuite(result.suite);
+    write('ok ' + ++numbers[browser.id] + ' ' + result.description + EOL);
   };
 
   this.specFailure = function(browser, result) {
-    write("not ok " + ++numbers[browser.id] + " " + result.suite.join(' ').replace(/\./g, '_') + " " + result.description + EOL);
+    var resultLog = JSON.parse(result.log[0]);
+    writeSuite(result.suite);
+    write('not ok ' + ++numbers[browser.id] + ' ' + result.description + EOL);
+    write('  ---' + EOL);
+    for (var key in resultLog) {
+      write('    ' + key + ': ' + resultLog[key] + EOL);
+    }
+    write('  ...' + EOL);
   };
 
   this.specSkipped = function(browser, result) {
-    write("ok " + ++numbers[browser.id] + " " + "# skip " + result.suite.join(' ').replace(/\./g, '_') + " " + result.description + EOL);
+    write('# SKIP' + ' ' + result.description);
   };
 
   this.onRunComplete = function(browsers, results) {
-    var total = 0;
+    var total = 0, success = 0, failed = 0;
     browsers.forEach(function(browser, id) {
       total += browser.lastResult.total;
+      success += browser.lastResult.success;
+      failed += browser.lastResult.failed;
     });
-    write("1.." + total + EOL);
 
-  if (outputFile) {
-			helper.mkdirIfNotExists(path.dirname(outputFile), function (err) {
-				if (err) {
-					return log.error('error writing report to file: ' + err);
-				}
-				log.info('writing report to file: ' + outputFile);
-				fs.writeFileSync(outputFile, output);
-			});
-		}
+    write(EOL);
+    write('1..' + total + EOL);
+    write('# tests ' + total + EOL);
+    write('# pass  ' + success + EOL);
+    if (failed) {
+      write('# fail  ' + failed + EOL);
+    }
+    write(EOL);
+    if (!failed) {
+      write('# ok' + EOL);
+    }
+    write(EOL);
+
+    if (outputFile) {
+      helper.mkdirIfNotExists(path.dirname(outputFile), function (err) {
+        if (err) {
+          return log.error('error writing report to file: ' + err);
+        }
+        log.info('writing report to file: ' + outputFile);
+        fs.writeFileSync(outputFile, output);
+      });
+    }
   };
 };
 
